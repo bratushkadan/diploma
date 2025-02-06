83af244ec6d724a7de73c5bce040f1c3f27b17d4
bratushkadan <bratushkadan@gmail.com>
2025-02-06 10:58:08 +0300

    feat(migrations):update ydb migration

--- migrations/auth/ydb/20250204185657_00001_create_first_table.sql
+++ migrations/auth/ydb/20250204185657_00001_create_first_table.sql
@@ -1,27 +1,27 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE users (
+CREATE TABLE accounts (
     id BIGSERIAL,
     name Utf8 NOT NULL,
     password Utf8 NOT NULL,
     email Utf8 NOT NULL,
     type String NOT NULL,
     created_at Timestamp NOT NULL,
-    activated Bool DEFAULT false,
+    activated_at Datetime,
     PRIMARY KEY (id),
-    INDEX idx_email_uniq GLOBAL UNIQUE on(email),
+    INDEX idx_email_uniq GLOBAL UNIQUE SYNC on(email),
 );
 CREATE TABLE refresh_tokens (
     id BIGSERIAL,
     user_id Int64 NOT NULL,
     created_at TIMESTAMP NOT NULL,
     expires_at TIMESTAMP NOT NULL,
-    PRIMARY KEY (id, user_id)
+    PRIMARY KEY (id, user_id),
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE users;
+DROP TABLE accounts;
 DROP TABLE refresh_tokens;
 -- +goose StatementEnd
