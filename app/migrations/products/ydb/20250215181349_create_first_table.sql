65119ea50e4a33290553f9cd8390c15190afdd91
bratushkadan <bratushkadan@gmail.com>
2025-02-15 21:27:05 +0300

    feat(products):prepare db migration schema (raw)

---
+++ app/migrations/products/ydb/20250215181349_create_first_table.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE products (
+    id Uuid,
+    seller_id Utf8,
+    name Utf8 NOT NULL,
+    description Utf8,
+    picture_urls List<String> NOT NULL,
+    stock Uint32 NOT NULL,
+    created_at Datetime NOT NULL,
+    updated_at Datetime,
+    PRIMARY KEY (id),
+    INDEX idx_seller_id on(seller_id),
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE products;
+-- +goose StatementEnd
