f0741a38dc548306e9bb2b8ff607313d741e2695
bratushkadan <bratushkadan@gmail.com>
2025-02-23 06:45:51 +0300

    fix(products):update db schema

--- app/migrations/products/ydb/20250215181349_create_first_table.sql
+++ app/migrations/products/ydb/20250215181349_create_first_table.sql
@@ -5,24 +5,16 @@ CREATE TABLE `products/products` (
     seller_id Utf8 NOT NULL,
     name Utf8 NOT NULL,
     description Utf8,
+    pictures Json NOT NULL,
     stock Uint32 NOT NULL,
     created_at Datetime NOT NULL,
     updated_at Datetime,
     PRIMARY KEY (id),
     INDEX idx_seller_id GLOBAL ASYNC ON (seller_id),
 );
-CREATE TABLE `products/product_images` (
-    product_id Uuid NOT NULL,
-    url Utf8 NOT NULL,
-    created_at Datetime NOT NULL,
-    deleted_at Datetime,
-    PRIMARY KEY (product_id, url)
-
-);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE `products/products`;
-DROP TABLE `products/product_images`;
 -- +goose StatementEnd
